6a5347df589c98ff59a2b02b3fc62b4e8d354f91
Michael Straeubig <android@i3games.de>
2012-10-15 12:34:21 +0200

    action bar "hotfix" for v-14

--- src/de/i3games/piepsendroid/PiepsenDroidActivity.java
+++ src/de/i3games/piepsendroid/PiepsenDroidActivity.java
@@ -182,9 +182,7 @@ public class PiepsenDroidActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		getWindow().addFlags(LayoutParams.FLAG_KEEP_SCREEN_ON);
 
 		setContentView(R.layout.main);
